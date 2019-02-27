be7a012fe0777fa4896e2862c9e9a608dd726271
Spencer Stewart <spencer.stewart@ad.snow.edu>
2019-02-27 11:22:39 -0700

    Update

--- HuffmanCoding.py
+++ HuffmanCoding.py
@@ -5,3 +5,13 @@ Created on Wed Feb 27 10:36:29 2019
 @author: spencer.stewart
 """
 
+"""
+
+You always connect the two smallest frequency nodes together.
+And then create a new node that connects bother of them.
+then look at the next lowest two and connect them.
+
+
+
+"""
+
