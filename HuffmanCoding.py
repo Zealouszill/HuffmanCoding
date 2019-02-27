cba8567d283e2f8e7a462b67d34cb34d7ca32860
S. Stewart <rubix.this@gmail.com>
2019-02-27 16:31:46 -0700

    Implemented the BinTreeNode

--- HuffmanCoding.py
+++ HuffmanCoding.py
@@ -24,14 +24,16 @@ then look at the next lowest two and connect them.
 
 class BinTreeNode:
 
-    def __init__(self, data = None, parent = None, leftChild = None, rightChild = None):
+    def __init__(self, data = None, frequency = None, parent = None, leftChild = None, rightChild = None):
 
         self._data = data
+        self._frequency = frequency
         self._parentNode = parent
         self._leftChild = leftChild
         self._rightChild = rightChild
 
     _data = 0
+    _frequency = 0
     _parentNode = 0
     _leftChild = 0
     _rightChild = 0
@@ -48,7 +50,7 @@ class TreeCodec:
 def convertSetenceToFrequencyList(sentence):
 
     tempFrequencyList = defaultdict(int)
-    frequencyList = defaultdict(list)
+    frequencyList = defaultdict(BinTreeNode)
 
     aSentence = sentence.lower()
 
@@ -59,11 +61,32 @@ def convertSetenceToFrequencyList(sentence):
         # print("printing", w , tempFrequencyList[w])
 
     for w in tempFrequencyList:
-        frequencyList[counter].extend([w, tempFrequencyList[w]])
+        frequencyList[counter] = BinTreeNode(w, tempFrequencyList[w], None, None, None)
+        #frequencyList[counter].extend([w, tempFrequencyList[w]])
         # print("printing this", w, frequencyList[counter])
         counter += 1
 
     return frequencyList
+
+# def convertSetenceToFrequencyList(sentence):
+#
+#     tempFrequencyList = defaultdict(int)
+#     frequencyList = defaultdict(list)
+#
+#     aSentence = sentence.lower()
+#
+#     counter = 0
+#
+#     for w in aSentence:
+#         tempFrequencyList[w] += 1
+#         # print("printing", w , tempFrequencyList[w])
+#
+#     for w in tempFrequencyList:
+#         frequencyList[counter].extend([w, tempFrequencyList[w]])
+#         # print("printing this", w, frequencyList[counter])
+#         counter += 1
+#
+#     return frequencyList
 
 
 
@@ -79,10 +102,10 @@ def test_SentenceConversion():
 
     frequencyList = convertSetenceToFrequencyList(aSentence)
 
-    assert frequencyList[0][0] == 'h'
-    assert frequencyList[0][1] == 3
-    assert frequencyList[1][0] == 'e'
-    assert frequencyList[1][1] == 2
+    assert frequencyList[0]._data == 'h'
+    assert frequencyList[0]._frequency == 3
+    assert frequencyList[1]._data == 'e'
+    assert frequencyList[1]._frequency == 2
 
 
 def test_fequencyListCreation():
