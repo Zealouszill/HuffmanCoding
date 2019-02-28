c628bfabd0c9bb727d11d8312ade7b7f8a539a22
Spencer Stewart <spencer.stewart@ad.snow.edu>
2019-02-28 01:06:32 -0700

    Push back to main

--- HuffmanCoding.py
+++ HuffmanCoding.py
@@ -21,6 +21,25 @@ then look at the next lowest two and connect them.
 
 """
 
+class Stack:
+     def __init__(self):
+         self.items = []
+
+     def isEmpty(self):
+         return self.items == []
+
+     def push(self, item):
+         self.items.append(item)
+
+     def pop(self):
+         return self.items.pop()
+
+     def peek(self):
+         return self.items[len(self.items)-1]
+
+     def size(self):
+         return len(self.items)
+
 
 class BinTreeNode:
 
@@ -71,16 +90,55 @@ def convertSetenceToFrequencyList(sentence):
     for w in tempFrequencyList:
         frequencyList[counter] = BinTreeNode(w, tempFrequencyList[w], None, None, None)
         #frequencyList[counter].extend([w, tempFrequencyList[w]])
-        # print("printing this", w, frequencyList[counter])
+        print("printing this", w, frequencyList[counter]._frequency)
         counter += 1
 
     return frequencyList
 
 
+def createFrequencyTree(frequencyList):
+    
+    lowestFreqency = BinTreeNode
+    secondLowestFreqency = BinTreeNode
+    
+    pq = PriorityQueue()
+    lowestFrequency = frequencyList[0]
+    
+    stack = []
+    
+    counter = 0
+    
+    for w in frequencyList:
+        
+        if frequencyList[w]._frequency < lowestFrequency._frequency:
+            secondLowestFrequency = lowestFrequency
+            lowestFrequency = frequencyList[counter]
+            counter += 1
+    
+
+    for w in frequencyList:
+        pq.add(frequencyList[w], frequencyList[w]._frequency)
+    
+    for w in frequencyList:
+        stack.append(pq.pop())
+        
+    
+    
+    
+    return stack.pop()._frequency
+
 ##############################################################
 #                            Tests                           #
 ##############################################################
 
+
+def test_CreateFrequencyTree():
+    
+    aSentence = "Hello how can I help you"
+
+    frequencyList = convertSetenceToFrequencyList(aSentence)
+    
+    assert createFrequencyTree(frequencyList) == 1
 
 
 def test_SentenceConversion():
@@ -93,6 +151,7 @@ def test_SentenceConversion():
     assert frequencyList[0]._frequency == 3
     assert frequencyList[1]._data == 'e'
     assert frequencyList[1]._frequency == 2
+    #assert frequencyList[1]._frequency == 1
 
 
 def test_fequencyListCreation():
@@ -103,3 +162,4 @@ def test_fequencyListCreation():
 
     assert frequencyList[0][0] == 'a'
     assert frequencyList[0][1] == 5
+
