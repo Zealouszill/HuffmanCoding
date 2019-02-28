ad4327df2b8ab08b90139785afd3a857682e6659
S. Stewart <rubix.this@gmail.com>
2019-02-28 15:14:09 -0700

    Before hack and slash

--- HuffmanCoding.py
+++ HuffmanCoding.py
@@ -18,27 +18,30 @@ And then create a new node that connects bother of them.
 then look at the next lowest two and connect them.
 
 
+Have a node class that asks the parent if the chidl node is the right child or the left child
+hashtable with the character, pointing to the leaf, and then have that leaf return the traverse up the tree
+to get the path for that node.
 
 """
 
-class Stack:
-     def __init__(self):
-         self.items = []
-
-     def isEmpty(self):
-         return self.items == []
-
-     def push(self, item):
-         self.items.append(item)
-
-     def pop(self):
-         return self.items.pop()
-
-     def peek(self):
-         return self.items[len(self.items)-1]
-
-     def size(self):
-         return len(self.items)
+# class Stack:
+#      def __init__(self):
+#          self.items = []
+#
+#      def isEmpty(self):
+#          return self.items == []
+#
+#      def push(self, item):
+#          self.items.append(item)
+#
+#      def pop(self):
+#          return self.items.pop()
+#
+#      def peek(self):
+#          return self.items[len(self.items)-1]
+#
+#      def size(self):
+#          return len(self.items)
 
 
 class BinTreeNode:
@@ -51,20 +54,22 @@ class BinTreeNode:
         self._leftChild = leftChild
         self._rightChild = rightChild
 
-    _data = 0
-    _frequency = 0
-    _parentNode = 0
-    _leftChild = 0
-    _rightChild = 0
+    def leafNodes(self):
+
+        if self._leftChild is None and self._rightChild is None:
+            yield self
+        else:
+            yield from self._leftChild.leafNodes()
+            yield from self._rightChild.leafNodes()
+
+
 
 
 class TreeCodec:
 
     def __init__(self, root):
         self._root = root
-
-    _root = BinTreeNode
-    _leaves = defaultdict(BinTreeNode)
+        self._leaves = dict(BinTreeNode)
 
     def encode(self):
         pass
@@ -105,53 +110,136 @@ def createFrequencyTree(frequencyList):
     lowestFrequency = frequencyList[0]
     
     stack = []
-    
+
+    print("empty stack:", len(stack))
+
+
     counter = 0
     
-    for w in frequencyList:
-        
-        if frequencyList[w]._frequency < lowestFrequency._frequency:
-            secondLowestFrequency = lowestFrequency
-            lowestFrequency = frequencyList[counter]
-            counter += 1
+    # for w in frequencyList:
+    #     print("This is w:", w)
+    #     if frequencyList[w]._frequency < lowestFrequency._frequency:
+    #         secondLowestFrequency = lowestFrequency
+    #         lowestFrequency = frequencyList[counter]
+    #         counter += 1
     
+
+
+    print("The length of frequencyLists is:", range(1, len(frequencyList)))
 
     for w in frequencyList:
         pq.add(frequencyList[w], frequencyList[w]._frequency)
-    
-    for w in frequencyList:
-        stack.append(pq.pop())
-        
-    
-    
-    
+        #pq.add(frequencyList[w])
+
+    # print("Now gunna pop")
+    # print(pq.pop()._data)
+    # print(pq.pop()._data)
+    # print(pq.pop()._data)
+    # print(pq.pop()._data)
+
+
+    stack = theStackMaker(pq)
+
+    counter = 0
+
+    print("The length of the stack is:", len(stack))
+
+    while len(stack) != 0:
+    # while counter < 2*len(frequencyList)-1:
+    # while counter < 2:
+
+        if len(stack) == 1:
+            i = stack.pop()
+
+            print("This is the tree", i._frequency)
+            print("This is the tree", i._leftChild._frequency)
+            print("This is the tree", i._rightChild._frequency)
+            print("This is the tree", i._leftChild._parentNode._frequency)
+
+            return i
+
+        else:
+            i = stack.pop()
+            j = stack.pop()
+
+
+
+            tempParentNode = BinTreeNode(i._frequency + j._frequency, i._frequency + j._frequency, None, i, j)
+
+            i._parentNode = tempParentNode
+            j._parentNode = tempParentNode
+
+            pq.add(tempParentNode, tempParentNode._frequency)
+
+            printOutContents(stack)
+
+            stack = theStackMaker(pq)
+
+    counter += 1
+
+
+
+
     return stack.pop()._frequency
+
+def printOutContents(itemsToPrint):
+
+    counter = 0
+
+    while counter < len(itemsToPrint):
+        print("Items to print", itemsToPrint)
+        counter += 1
+
+def theStackMaker(priorityQueue):
+
+    stack = []
+    counter = 0
+
+    while counter < len(priorityQueue):
+        stack.append(priorityQueue.pop())
+
+    return stack
+
+def traverseTree(tree):
+
+    for w in len(tree):
+        print("This is the tree:", w, tree[w]._data)
+        print("This is the node frequency:", w, tree[w].frequency)
+
+
 
 ##############################################################
 #                            Tests                           #
 ##############################################################
 
 
+
 def test_CreateFrequencyTree():
-    
-    aSentence = "Hello how can I help you"
+
+    # aSentence = "Hello how can I help you"
+    aSentence = "Hello"
 
     frequencyList = convertSetenceToFrequencyList(aSentence)
-    
-    assert createFrequencyTree(frequencyList) == 1
+
+    TreeCodec = createFrequencyTree(frequencyList)
+
+    # traverseTree(TreeCodec)
+
+    assert TreeCodec._data == 5
+    assert TreeCodec._frequency == 5
 
 
 def test_SentenceConversion():
 
-    aSentence = "Hello how can I help you"
+    aSentence = "Hello"
 
     frequencyList = convertSetenceToFrequencyList(aSentence)
 
     assert frequencyList[0]._data == 'h'
-    assert frequencyList[0]._frequency == 3
+    assert frequencyList[0]._frequency == 1
     assert frequencyList[1]._data == 'e'
-    assert frequencyList[1]._frequency == 2
-    #assert frequencyList[1]._frequency == 1
+    assert frequencyList[1]._frequency == 1
+    assert frequencyList[1]._frequency == 1
 
 
 def test_fequencyListCreation():
